714486dbc526428ac43e7d928ff0fc6f633eba1d
Minh Tung <phamminhtung111097@gmail.com>
2016-11-02 17:21:18 +0700

    init project

---
+++ ADPdemo/src/ADPdemo.java
@@ -0,0 +1,31 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author Tung Pham
+ *
+ */
+public class ADPdemo extends JFrame {
+	public ADPdemo (){
+		setTitle("ADP");
+		setSize(500, 500);
+		setLayout(null);
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ADPdemo f = new ADPdemo();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+	}
+
+}
